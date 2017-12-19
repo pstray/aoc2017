a6e467f05bc7b4d0fb19e295dd1b029c1c05acbb
Peder Stray <peder.stray@gmail.com>
2017-12-20 00:08:29 +0100

    Solution to 16/2

--- 16/solution.pl
+++ 16/solution.pl
@@ -8,28 +8,44 @@ use Data::Dumper;
 
 my $input = do { local $/ = undef; <>; };
 chomp $input;
-
 my $programs = join "", 'a' .. 'p';
+my %cache = ( $programs => 1 );
+my @cache = ($programs);
 
-for my $move (split ",", $input) {
-    if ($move =~ m,^s(\d+)$,) {
-	substr $programs, 0, 0, substr $programs, -$1, $1, '';
+my $max_iter = 1_000_000_000;
+
+for (1 .. $max_iter) {
+    for my $move (split ",", $input) {
+	if ($move =~ m,^s(\d+)$,) {
+	    substr $programs, 0, 0, substr $programs, -$1, $1, '';
+	}
+	elsif ($move =~ m,^x(\d+)/(\d+),) {
+	    (substr($programs,$1,1),substr($programs,$2,1)) =
+	      (substr($programs,$2,1),substr($programs,$1,1));
+	}
+	elsif ($move =~ m,^p(.)/(.),) {
+	    my $p1 = index $programs, $1;
+	    my $p2 = index $programs, $2;
+	    (substr($programs,$p1,1),substr($programs,$p2,1)) =
+	      (substr($programs,$p2,1),substr($programs,$p1,1));
+	}
+	else {
+	    die "Unhandled move [$move]\n";
+	}
     }
-    elsif ($move =~ m,^x(\d+)/(\d+),) {
-	(substr($programs,$1,1),substr($programs,$2,1)) =
-	  (substr($programs,$2,1),substr($programs,$1,1));
-    }
-    elsif ($move =~ m,^p(.)/(.),) {
-	my $p1 = index $programs, $1;
-	my $p2 = index $programs, $2;
-	(substr($programs,$p1,1),substr($programs,$p2,1)) =
-	  (substr($programs,$p2,1),substr($programs,$p1,1));
-    }
-    else {
-	die "Unhandled move [$move]\n";
-    }
+    last if $cache{$programs}++;
+    push @cache, $programs;
 }
 
+print Dumper \@cache;
 
-printf "$programs\n";
+printf "Solution 1: %s\n", $cache[1];
+printf "Cycle length: %d\n", 0+@cache;
+
+my $end_pos = ($max_iter) % @cache;
+printf "End pos: %s\n", $end_pos;
+
+printf "Solution 2: %s\n", $cache[$end_pos];
+
+
 
