3e3e494d6ca2e1f6c17982baee0dd5b0cb1744e1
Peder Stray <peder@inne.proxdynamics.com>
2017-12-20 16:07:09 +0100

    Solution to 05/2

--- 05/solution.pl
+++ 05/solution.pl
@@ -11,6 +11,8 @@ while (<>) {
     push @jumps, $_;
 }
 
+my @jumps2 = @jumps;
+
 my $ip = 0;
 my $cx = 0;
 
@@ -23,3 +25,20 @@ while ($ip < @jumps) {
 
 printf "Solution 1: %d jumps to escape\n", $cx;
 
+$ip = $cx = 0;
+
+#printf "%s\n", join " ", map { $_==$ip ? "($jumps2[$_])" : " $jumps2[$_] " } 0 .. @jumps2-1;
+while ($ip < @jumps2) {
+    $cx++;
+    my $offset = $jumps2[$ip];
+    if ($offset >= 3) {
+	$jumps2[$ip]--;
+    }
+    else {
+	$jumps2[$ip]++;
+    }
+    $ip += $offset;
+    #printf "%s\n", join " ", map { $_==$ip ? "($jumps2[$_])" : " $jumps2[$_] " } 0 .. @jumps2-1;
+}
+
+printf "Solution 2: %d jumps to escape\n", $cx;
