1215877ea7f2fabc132ebb69e7c2a1bdacbf4f2a
Peder Stray <peder@inne.proxdynamics.com>
2017-12-20 15:16:44 +0100

    Solution to 04/2

--- 04/solution.pl
+++ 04/solution.pl
@@ -5,14 +5,20 @@ use strict;
 @ARGV = qw(input) unless @ARGV;
 
 my $valid_passwords1 = 0;
+my $valid_passwords2 = 0;
+
 while (<>) {
     chomp;
-    my(%words1);
+    my(%words1,%words2);
     my $valid1 = 1;
+    my $valid2 = 1;
     for my $word (split " ") {
 	$valid1 = 0 if ++$words1{$word} > 1;
+	$valid2 = 0 if ++$words2{join "", sort split "", $word} > 1;
     }
     $valid_passwords1++ if $valid1;
+    $valid_passwords2++ if $valid1 && $valid2;
 }
 
 printf "Solution 1: %d\n", $valid_passwords1;
+printf "Solution 2: %d\n", $valid_passwords2;
