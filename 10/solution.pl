e682a830e6541ab63259f07444300d640f5aeecf
Peder Stray <peder.stray@gmail.com>
2017-12-21 18:24:19 +0100

    Solution to 10/2

--- 10/solution.pl
+++ 10/solution.pl
@@ -4,26 +4,66 @@ use strict;
 
 @ARGV = qw(input) unless @ARGV;
 
-my @lengths = split ",", do { local $/ = undef; <>; };
-my @list = (0 .. 255);
+my $input = do { local $/ = undef; <>; };
+chomp $input;
+my @lengths = split ",", $input;
 
-my $pos = 0;
-my $skip = 0;
+sub knot_hash_1 {
+    my @lengths = @_;
+    my @list = (0 .. 255);
+    my $pos = 0;
+    my $skip = 0;
 
-#printf "%s\n", join " ", map { $_==$pos ? "($list[$_])" : " $list[$_] " } 0 .. @list-1;
-for my $length (@lengths) {
-    die "invalid length $length\n" if $length > @list;
+    for my $length (@lengths) {
+	push @list, splice @list, 0, $pos;
+	splice @list, 0, $length, reverse @list[0 .. $length-1];
+	unshift @list, splice @list, -$pos;
 
-    push @list, splice @list, 0, $pos;
-    splice @list, 0, $length, reverse @list[0 .. $length-1];
-    unshift @list, splice @list, -$pos;
-    
-    $pos += $length + $skip;
-    $pos %= 0+@list;
-    $skip++;
-
-    #printf "%s\n", join " ", map { $_==$pos ? "($list[$_])" : " $list[$_] " } 0 .. @list-1;
-
+	$pos += $length + $skip;
+	$pos %= 0+@list;
+	$skip++;
+    }
+    return @list;
 }
 
+die "invalid length detected\n"
+  if grep { $_ > 255 } @lengths;
+
+my @list = knot_hash_1(@lengths);
+
 printf "Solution 1: %d\n", $list[0]*$list[1];
+
+sub knot_hash {
+    my($input) = @_;
+    my @input = unpack "c*", $input;
+    my @sparse_hash = (0 .. 255);
+    my $pos = 0;
+    my $skip = 0;
+
+    push @input, 17, 31, 73, 47, 23;
+
+    for my $round (1 .. 64) {
+	for my $length (@input) {
+	    push @sparse_hash, splice @sparse_hash, 0, $pos;
+	    splice @sparse_hash, 0, $length, reverse @sparse_hash[0 .. $length-1];
+	    unshift @sparse_hash, splice @sparse_hash, -$pos;
+	    $pos += $length + $skip;
+	    $pos %= 256;
+	    $skip++;
+	}
+    }
+
+    my @dense_hash;
+    for my $i (0 .. 255) {
+	push @dense_hash, 0 unless $i % 16;
+	$dense_hash[-1] ^= $sparse_hash[$i];
+    }
+
+    return unpack "H*", pack "c*", @dense_hash;
+}
+
+for my $test ("", "AoC 2017", "1,2,3", "1,2,4") {
+    printf "%-10s: %s\n", "'$test'", knot_hash($test);
+}
+
+printf "Solution 2: %s\n", knot_hash($input);
