29d0bf8275fb5bb8459b5a3973920aac926c4395
Peder Stray <peder.stray@gmail.com>
2017-12-22 02:30:53 +0100

    Not working yet...

--- 13/solution.pl
+++ 13/solution.pl
@@ -37,3 +37,98 @@ for my $depth (0 .. @wall) {
 }
 
 printf "Solution 1: severity of %s\n", $severity;
+
+sub reset_wall {
+    for (values %wall) {
+	$_->{dir} = 1;
+	$_->{pos} = 0;
+    }
+}
+
+sub update_wall {
+    for my $w (values %wall) {
+	$w->{pos} += $w->{dir};
+	if ($w->{pos} == $w->{range}-1 || $w->{pos} == 0) {
+	    $w->{dir} *= -1;
+	}
+    }
+}
+
+my $done = 0;
+my $delay = 0;
+my $time = 0;
+
+$| = 1;
+
+travers:
+while (!$done) {
+
+    reset_wall();
+
+    my $depth = -1;
+
+    printf "Traversing from depth=%d, delay=%d...\n", $depth, $delay;
+
+    while ($depth < @wall) {
+	draw_wall(0, $depth);
+	$time++;
+	$depth++;
+
+	my $layer = $wall[$depth];
+	if ($layer) {
+	    if ($layer->{pos} == 0) {
+		$delay++;
+		$depth--;
+	    }
+	}
+	
+	draw_wall(0, $depth);
+
+	update_wall();
+
+    }
+
+    $done++;
+}
+
+printf "Solution 2: delay %d\n", $delay;
+
+sub draw_wall {
+    my($start, $depth) = @_;
+    $start = 0 if $start < 0;
+    printf "\e[H\e[2J";
+    printf "D=%d T=%d\n", $depth, $time;
+    for my $d ($start .. $start + 15) {
+	printf " %3d", $d;
+    }
+    print "\n";
+    for my $line (0 .. 19) {
+	for my $d ($start .. $start + 15) {
+	    my $w = $wall[$d];
+	    my $s = " ";
+	    if ($w) {
+		$s = "S" if $w->{pos} == $line;
+		if ($d == $depth && $line == 0) {
+		    $s = "($s)";
+		}
+		elsif ($line < $w->{range}) {
+		    $s = "[$s]";
+		}
+		else {
+		    $s = " $s ";
+		}
+	    }
+	    else {
+		if ($d == $depth && $line == 0) {
+		    $s = "($s)";
+		}
+		else {
+		    $s = " $s ";
+		}
+	    }
+	    printf " %s", $s;
+	}
+	printf "\n";
+    }
+    sleep 1 if $start <= $depth && $depth <= $start+15;
+}
