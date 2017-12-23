8d41e558947c57aa5dc8790fe6b76f91f202a48d
Peder Stray <peder.stray@gmail.com>
2017-12-23 09:33:32 +0100

    Solution to 23/1

---
+++ 23/solution.pl
@@ -0,0 +1,53 @@
+#! /usr/bin/perl
+
+use strict;
+
+use Data::Dumper;
+use Time::HiRes qw(sleep);
+
+$Data::Dumper::Sortkeys = $Data::Dumper::Indent = 1;
+
+@ARGV = qw(input) unless @ARGV;
+
+my @code;
+while (<>) {
+    chomp;
+    push @code, [ split " ", $_ ];
+}
+
+my $ip = 0;
+my %reg;
+my $freq = 0;
+
+my $solution1 = 0;
+
+while ($ip < @code) {
+    my($op,$x,$y) = @{$code[$ip]};
+    my $inc = 1;
+
+    $y = $reg{$y} ||= 0 if $y =~ /[^0-9-]/;
+    
+    if ($op eq 'set') {
+	$reg{$x} = $y;
+    }
+    elsif ($op eq 'sub') {
+	$reg{$x} -= $y;
+    }
+    elsif ($op eq 'mul') {
+	$reg{$x} *= $y;
+	$solution1++;
+    }
+    elsif ($op eq 'jnz') {
+	$x = $reg{$x} ||= 0 if $x =~ /[^0-9-]/;
+	if ($x != 0) {
+	    $inc = $y;
+	}
+    }
+    else {
+	die "unknown op [$op] $x $y ($code[$ip][2])\n", join " ", $op, $x, $y;
+    }
+    $ip += $inc;
+}
+
+printf "Solution 1: %d\n", $solution1;
+
