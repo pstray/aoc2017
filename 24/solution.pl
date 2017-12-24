c7266f9662cde4ce87d9d677ba8fa5e40b079d30
Peder Stray <peder.stray@gmail.com>
2017-12-24 10:20:07 -0400

    Solution to 24/1

--- 24/solution.pl
+++ 24/solution.pl
@@ -8,7 +8,6 @@ $Data::Dumper::Sortkeys = $Data::Dumper::Indent = 1;
 @ARGV = qw(input) unless @ARGV;
 
 my @input;
-
 while (<>) {
     chomp;
     if (m,^(\d+)/(\d+),) {
@@ -17,24 +16,34 @@ while (<>) {
 }
 
 sub find_max_bridge {
-    my($ports) = @_;
-    my $sum;
+    my($ports,$components,$level) = @_;
+    my $max_sum = 0;
 
-    my @unused = grep { !$used{$->[2]} } @$ports;
+    my @usable = grep { $_->[0] == $ports || $_->[1] == $ports }
+      grep { !$_->[2] } @$components;
 
-    for my $port (@unused) {
-	$port->[2] = 1;
-	my $sub = find_max_bridge($ports);
-	
-	
+#    printf("%sUsable with %d pins, %d = %s\n",
+#	   " "x$level,
+#	   $ports,
+#	   0+@usable,
+#	   join(" ", map { $_->[0] . "/" . $_->[1] } @usable)
+#	  );
+
+    for my $c (@usable) {
+#	printf "%s Using %d/%d\n", " "x$level, $c->[0], $c->[1];
+	       
+	$c->[2] = 1;
+	my $other = $c->[0] == $ports ? $c->[1] : $c->[0];
+	my $sum =  $c->[0] + $c->[1] + find_max_bridge($other,$components,$level+1);
+
+	$max_sum = $sum if $sum > $max_sum;
+	$c->[2] = 0;
     }
 
+    return $max_sum;
     
-    
-    
-    
-    
-
-    
-
 }
+
+my $max = find_max_bridge(0,\@input);
+
+printf "Solution 1: %d\n", $max;
