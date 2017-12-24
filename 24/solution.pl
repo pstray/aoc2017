35cdd90c2340d32f9b7a1fe1ef3ee73f4c77ed29
Peder Stray <peder.stray@gmail.com>
2017-12-24 10:32:50 -0400

    Solution to 24/2

--- 24/solution.pl
+++ 24/solution.pl
@@ -15,8 +15,10 @@ while (<>) {
     }
 }
 
+my @max_at_level;
+
 sub find_max_bridge {
-    my($ports,$components,$level) = @_;
+    my($ports,$components,$level,$sum_to_now) = @_;
     my $max_sum = 0;
 
     my @usable = grep { $_->[0] == $ports || $_->[1] == $ports }
@@ -34,16 +36,22 @@ sub find_max_bridge {
 	       
 	$c->[2] = 1;
 	my $other = $c->[0] == $ports ? $c->[1] : $c->[0];
-	my $sum =  $c->[0] + $c->[1] + find_max_bridge($other,$components,$level+1);
+	my $sum = $c->[0] + $c->[1];
+
+	$sum += find_max_bridge($other,$components,$level+1,$sum_to_now+$sum);
 
 	$max_sum = $sum if $sum > $max_sum;
+	$max_at_level[$level] = $sum+$sum_to_now
+	  if $sum+$sum_to_now > $max_at_level[$level];
 	$c->[2] = 0;
     }
 
     return $max_sum;
-    
 }
 
 my $max = find_max_bridge(0,\@input);
 
-printf "Solution 1: %d\n", $max;
+printf "Solution 1: max strength = %d\n", $max;
+
+printf "Solution 1: max length = %d, strengt of brigde = %d\n",
+  0+@max_at_level, $max_at_level[-1]
