f645cbf464a1c619c701cf472cf6f95d0daabcf9
Peder Stray <peder.stray@gmail.com>
2018-12-03 07:47:13 +0100

    Solution to 2018-03

---
+++ 2018/03/solution.pl
@@ -0,0 +1,41 @@
+#! /usr/bin/perl
+#
+#    $Id$
+#
+# Copyright (C) 2018 by Peder Stray <peder@wintermute>
+#
+
+use strict;
+
+my %data;
+my %ids;
+
+while (<>) {
+    chomp;
+    if (my ($id,$x,$y,$w,$h) = /#(\d+)\s+@\s+(\d+),(\d+):\s+(\d+)x(\d+)/) {
+	for my $xx ($x .. $x+$w-1) {
+	    for my $yy ($y .. $y+$h-1) {
+		push @{$data{$xx}{$yy}}, $id;
+		$ids{$id}++;
+	    }
+	}
+    }
+    else {
+	warn "Unparsable input: $_\n";
+    }
+}
+
+my $overlaps = 0;
+my @no_overlap_ids;
+for my $x (keys %data) {
+    for my $y (keys %{$data{$x}}) {
+	my @ids = @{$data{$x}{$y}};
+	if (@ids>1) {
+	    $overlaps++;
+	    delete $ids{$_} for @ids;
+	}
+    }
+}
+
+printf "Solution 1: %d\n", $overlaps;
+printf "Solution 2: %s\n", join ", ", keys %ids;
