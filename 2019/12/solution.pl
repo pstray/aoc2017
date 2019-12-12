b52d8b1960f3761da2340028cc1fa11da1e36a33
Peder Stray <peder.stray@gmail.com>
2019-12-12 07:04:21 +0100

    Simple attempt at 2019/12 #2

--- 2019/12/solution.pl
+++ 2019/12/solution.pl
@@ -20,12 +20,24 @@ my @object;
 while (<>) {
     chomp;
     if (/<x=(?<x>-?\d+), y=(?<y>-?\d+), z=(?<z>-?\d+)>/) {
-	push @object, { pos => { %+ } };
+	push @object, { pos => { %+ }, vel => { x => 0, y => 0, z => 0 } };
     }
     else {
 	die "Kablam! $_\n";
     }
 }
+
+my %seen;
+
+sub state {
+    my @state;
+    for my $o (@_) {
+	push @state, @{$o->{pos}}{qw(x y z)}, @{$o->{vel}}{qw(x y z)},
+    }
+    return join ",", @state;
+}
+
+$seen{state(@object)}++;
 
 my $time = 0;
 while ($time < 1000) {
@@ -50,6 +62,12 @@ while ($time < 1000) {
 	    $o->{pos}{$d} += $o->{vel}{$d};
 	}
     }
+
+    my $state = state(@object);
+
+    if ($seen{$state}++) {
+	$solution2 = $time;
+    }
 }
 
 for my $o (@object) {
@@ -63,7 +81,38 @@ for my $o (@object) {
     $solution1 += $o->{total};
 }
 
-
 printf "Solution 1: %s\n", join " ", $solution1;
+
+while (!$solution2) {
+    $time++;
+
+    for my $i (0 .. @object-1) {
+	for my $j ($i+1 .. @object-1) {
+	    for my $d (qw(x y z)) {
+		if ($object[$i]{pos}{$d} < $object[$j]{pos}{$d}) {
+		    $object[$i]{vel}{$d}++;
+		    $object[$j]{vel}{$d}--;
+		} elsif ($object[$i]{pos}{$d} > $object[$j]{pos}{$d}) {
+		    $object[$i]{vel}{$d}--;
+		    $object[$j]{vel}{$d}++;
+		}
+	    }
+	}
+    }
+
+    for my $o (@object) {
+	for my $d (qw(x y z)) {
+	    $o->{pos}{$d} += $o->{vel}{$d};
+	}
+    }
+
+    my $state = state(@object);
+
+    if ($seen{$state}++) {
+	$solution2 = $time;
+    }
+
+    printf "t: %d\n", $time unless $time % 1000;
+}
 
 printf "Solution 2: %s\n", join " ", $solution2;
