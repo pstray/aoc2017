c184240b4d1652c8f0517053eccd2fbf845f76e7
Peder Stray <peder.stray@gmail.com>
2019-12-16 09:15:33 +0100

    Need something smarter...

--- 2019/16/solution.pl
+++ 2019/16/solution.pl
@@ -21,8 +21,6 @@ my $solution1;
 my $solution2;
 
 my @data;
-my @in_pattern = (0, 1, 0, -1);
-
 while (<>) {
     chomp;
     if (/(\d+)/) {
@@ -37,15 +35,62 @@ while (<>) {
 my @input = @data;
 for my $phase (1 .. 100) {
     my @output;
-    for my $mult (1 .. @input) {
-	my @pattern;
-	for my $in (@in_pattern) {
-	    push @pattern, ($in) x $mult;
-	    last if @pattern > @input;
-	}
+    for my $o_index (0 .. @input-1) {
+	my $pos = $o_index;
 	my $output;
-	for my $i (0 .. @input-1) {
-	    $output += $input[$i] * $pattern[($i+1) % @pattern];
+      POS:
+	while (1) {
+	    for (0..$o_index) {
+		last POS unless $pos < @input;
+		$output += $input[$pos++];
+	    }
+	    $pos += $o_index+1;
+	    for (0..$o_index) {
+		last POS unless $pos < @input;
+		$output -= $input[$pos++];
+	    }
+	    $pos += $o_index+1;
+	}
+	push @output, abs($output)%10;
+    }
+    # printf "%s\n", join "", @output;
+    @input = @output;
+}
+
+$solution1 = join "", @input[0..7];
+
+printf "Solution 1: %s\n", join " ", $solution1;
+
+# @input = @data x 10000
+
+# 0p0n0p0n0p0n
+#  ddd dddd dddd
+# 0pp00nn00pp00nn
+
+my @input = @data;
+my $input_size = @data * 10000;
+
+printf "Size: %s\n", $input_size;
+
+for my $phase (1 .. 100) {
+    my @output;
+    for my $o_index (0 .. $input_size-1) {
+	my $pos = $o_index;
+	my $output = 0;
+	my $m = lcm(0+@input,($o_index+1)*4);
+	printf "i=%d: m=%s\n", $o_index, $m;
+      POS:
+	while (1) {
+	    for (0..$o_index) {
+		last POS unless $pos < $m;
+		$output += $input[$pos++ % @input];
+	    }
+	    $pos += $o_index+1;
+	    for (0..$o_index) {
+		last POS unless $pos < $m;
+		$output -= $input[$pos++ % @input];
+	    }
+	    $pos += $o_index+1;
 	}
 	push @output, abs($output)%10;
     }
@@ -53,7 +98,9 @@ for my $phase (1 .. 100) {
     @input = @output;
 }
 
-$solution1 = join "", @input[0..7];
+my $offset = join "", @data[0..6];
+for my $o ($offset .. $offset+6) {
+    $solution2 .= $input[$o%@input];
+}
 
-printf "Solution 1: %s\n", join " ", $solution1;
 printf "Solution 2: %s\n", join " ", $solution2;
