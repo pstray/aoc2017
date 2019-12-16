d219764a53b1f5edc0ff49bce071be95ad7de396
Peder Stray <peder.stray@gmail.com>
2019-12-16 22:53:05 +0100

    Solution for 2019/16

--- 2019/16/solution.pl
+++ 2019/16/solution.pl
@@ -61,46 +61,41 @@ $solution1 = join "", @input[0..7];
 
 printf "Solution 1: %s\n", join " ", $solution1;
 
-# @input = @data x 10000
-
-# 0p0n0p0n0p0n
-#  ddd dddd dddd
-# 0pp00nn00pp00nn
-
-my @input = @data;
-my $input_size = @data * 10000;
-
-printf "Size: %s\n", $input_size;
-
-for my $phase (1 .. 100) {
-    my @output;
-    for my $o_index (0 .. $input_size-1) {
-	my $pos = $o_index;
-	my $output = 0;
-	my $m = lcm(0+@input,($o_index+1)*4);
-	printf "i=%d: m=%s\n", $o_index, $m;
-      POS:
-	while (1) {
-	    for (0..$o_index) {
-		last POS unless $pos < $m;
-		$output += $input[$pos++ % @input];
-	    }
-	    $pos += $o_index+1;
-	    for (0..$o_index) {
-		last POS unless $pos < $m;
-		$output -= $input[$pos++ % @input];
-	    }
-	    $pos += $o_index+1;
-	}
-	push @output, abs($output)%10;
-    }
-    printf "%s\n", join "", @output;
-    @input = @output;
-}
-
+my @input = (@data) x 10000;
+my $input_size = @input;
 my $offset = join "", @data[0..6];
-for my $o ($offset .. $offset+6) {
-    $solution2 .= $input[$o%@input];
+
+# For any index over halfway, n-1 is the sum of the previous n and n-1,
+# but for any index lower, it gets more complex... thus:
+#
+#  0 +_-_+_-_+_-_+_-_|+_-_+_-_+_-_+_-_|+_-_+_-_+_-_+_-_|+_-_+_-_+_-_+_-_|
+#  1 _++__--__++__--_|_++__--__++__--_|_++__--__++__--_|_++__--__++__--_|
+#  2 __+++___---___++|+___---___+++___|---___+++___---_|__+++___---___++|
+#  3 ___++++____----_|___++++____----_|___++++____----_|___++++____----_|
+#  4 ____+++++_____--|---_____+++++___|__-----_____++++|+_____-----_____|
+#  5 _____++++++_____|_------______+++|+++______------_|_____++++++_____|
+#  6 ______+++++++___|____-------_____|__+++++++_______|-------_______++|
+#  7 _______++++++++_|_______--------_|_______++++++++_|_______--------_|
+#  8 ________++++++++|+_________------|---_________++++|+++++_________--|
+#  9 _________+++++++|+++__________---|-------_________|_++++++++++_____|
+# 10 __________++++++|+++++___________|-----------_____|______++++++++++|
+# 11 ___________+++++|+++++++_________|___------------_|___________+++++|
+# 12 ____________++++|+++++++++_______|______----------|---_____________|
+# 13 _____________+++|+++++++++++_____|_________-------|-------_________|
+# 14 ______________++|+++++++++++++___|____________----|-----------_____|
+# 15 _______________+|+++++++++++++++_|_______________-|---------------_|
+
+die sprintf "Only valid for %s >= %s\n", $offset, $input_size/2
+  if $offset < $input_size/2;
+
+splice @input, 0, $offset;
+for my $phase (1 .. 100) {
+    # go from the end, so we dont overwrite a previous value we need
+    for (my $i = @input-1; $i > 0; $i--) {
+	$input[$i-1] = ($input[$i-1]+$input[$i]) % 10;
+    }
 }
+
+$solution2 = join "", @input[0..7];
 
 printf "Solution 2: %s\n", join " ", $solution2;
