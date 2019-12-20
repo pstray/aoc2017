9eeb81bf96490a062005b8309d63ece2fa71d5cc
Peder Stray <peder.stray@gmail.com>
2019-12-20 10:11:15 +0100

    Solution 2019/20 #2

--- 2019/20/solution.pl
+++ 2019/20/solution.pl
@@ -38,6 +38,31 @@ while (<>) {
     $y++;
 }
 
+$data->{__min_wall_x} = $data->{__max_x};
+$data->{__min_wall_y} = $data->{__max_y};
+$data->{__max_wall_x} = $data->{__min_x};
+$data->{__max_wall_y} = $data->{__min_y};
+
+for my $y (keys %{$data->{__map}}) {
+    for my $x (keys %{$data->{__map}{$y}}) {
+	if ($data->{__map}{$y}{$x} eq '#') {
+	    $data->{__max_wall_x} = $x if $data->{__max_wall_x} < $x;
+	    $data->{__max_wall_y} = $y if $data->{__max_wall_y} < $y;
+	    $data->{__min_wall_x} = $x if $data->{__min_wall_x} > $x;
+	    $data->{__min_wall_y} = $y if $data->{__min_wall_y} > $y;
+	}
+    }
+}
+
+sub inside {
+    my($self,$x,$y) = @_;
+    return 0 if $x < $self->{__min_wall_x};
+    return 0 if $y < $self->{__min_wall_y};
+    return 0 if $x > $self->{__max_wall_x};
+    return 0 if $y > $self->{__max_wall_y};
+    return 1;
+}
+
 for my $y (sort { $a <=> $b } keys %{$data->{__map}}) {
     for my $x (sort { $a <=> $b } keys %{$data->{__map}{$y}}) {
 	my $tile =  $data->{__map}{$y}{$x};
@@ -45,26 +70,30 @@ for my $y (sort { $a <=> $b } keys %{$data->{__map}}) {
 	    # UP
 	    if ($data->{__map}{$y-1}{$x} =~ /[A-Z]/) {
 		my @p = ($data->{__map}{$y-1}{$x}, $data->{__map}{$y-2}{$x});
+		my $i = inside($data,$x,$y-1);
 		my $p = join "", sort @p;
-		push @{$data->{__portal}{$p}}, [ $x, $y ];
+		$data->{__portal}{$p}[$i] = [ $x, $y, $i ];
 	    }
 	    # RIGHT
 	    if ($data->{__map}{$y}{$x+1} =~ /[A-Z]/) {
 		my @p = ($data->{__map}{$y}{$x+1}, $data->{__map}{$y}{$x+2});
+		my $i = inside($data,$x+1,$y);
 		my $p = join "", sort @p;
-		push @{$data->{__portal}{$p}}, [ $x, $y ];
+		$data->{__portal}{$p}[$i] = [ $x, $y, $i ];
 	    }
 	    # DOWN
 	    if ($data->{__map}{$y+1}{$x} =~ /[A-Z]/) {
 		my @p = ($data->{__map}{$y+1}{$x}, $data->{__map}{$y+2}{$x});
+		my $i = inside($data,$x,$y+1);
 		my $p = join "", sort @p;
-		push @{$data->{__portal}{$p}}, [ $x, $y ];
+		$data->{__portal}{$p}[$i] = [ $x, $y, $i ];
 	    }
 	    # LEFT
 	    if ($data->{__map}{$y}{$x-1} =~ /[A-Z]/) {
 		my @p = ($data->{__map}{$y}{$x-1}, $data->{__map}{$y}{$x-2});
+		my $i = inside($data,$x-1,$y);
 		my $p = join "", sort @p;
-		push @{$data->{__portal}{$p}}, [ $x, $y ];
+		$data->{__portal}{$p}[$i] = [ $x, $y, $i ];
 	    }
 	}
     }
@@ -112,7 +141,7 @@ sub draw_map {
     printf "\e[J\n";
 }
 
-draw_map($data);
+# draw_map($data);
 
 sub next_pos {
     my($x,$y,$d) = @_;
@@ -134,7 +163,7 @@ sub next_pos {
 my @explore = ($data->{__portal}{ZZ}[0]);
 my $depth = 0;
 
-EXPLORE:
+EXPLORE1:
 while (@explore) {
     my @next;
     # draw_map($data, "$depth ".join " ", map { $_->[0].",".$_->[1] } @explore);
@@ -151,9 +180,9 @@ while (@explore) {
 	    }
 	    elsif ($tile =~ /[A-Z]/) {
 		my($px,$py) = next_pos($nx,$ny,$dir);
-		my $pn = join "", sort $tile, $data->{__map}{$py}{$px};
-		last EXPLORE if $pn eq 'AA';
-		my $p = $data->{__portal}{$pn};
+		my $p_name = join "", sort $tile, $data->{__map}{$py}{$px};
+		last EXPLORE1 if $p_name eq 'AA';
+		my $p = $data->{__portal}{$p_name};
 		my($np) = grep { $_->[0] != $x || $_->[1] != $y } @$p;
 		if ($np) {
 		    $nx = $np->[0];
@@ -172,10 +201,78 @@ while (@explore) {
     # sleep .1;
 }
 
-draw_map($data, "done");
+# draw_map($data, "done");
 
 my($ex,$ey) = @{$data->{__portal}{AA}[0]};
 $solution1 = $data->{__depth}{$ey}{$ex};
 
 printf "Solution 1: %s\n", join " ", $solution1;
-printf "Solution 2: %s\n", join " ", $solution2;
+
+my @explore = ( map { [ @$_, 0 ] } $data->{__portal}{ZZ}[0]);
+my $steps = 0;
+my $max_level = 0;
+EXPLORE2:
+while (@explore) {
+    my @next;
+    # printf "step: %d\n", $steps;
+    # draw_map($data, "$depth ".join " ", map { $_->[0].",".$_->[1] } @explore);
+    for my $pos (@explore) {
+	my($x,$y,$level) = @$pos;
+	$max_level = $level if $level > $max_level;
+	next if $data->{__steps}[$level]{$y}{$x};
+	$data->{__steps}[$level]{$y}{$x} = $steps;
+	# print "  exploring $x,$y,$level\n";
+
+	for my $dir (0..3) {
+	    my($nx,$ny) = next_pos($x,$y,$dir);
+	    my $nl = $level;
+	    my $tile = $data->{__map}{$ny}{$nx};
+	    if ($tile eq '.') {
+		# ok
+	    }
+	    elsif ($tile =~ /[A-Z]/) {
+		my($px,$py) = next_pos($nx,$ny,$dir);
+		my $p_name = join "", sort $tile, $data->{__map}{$py}{$px};
+
+		if ($p_name eq 'ZZ') {
+		    # printf "    Portal ZZ... skipping\n";
+		    next;
+		}
+		if ($p_name eq 'AA') {
+		    last EXPLORE2 unless $level;
+		    next;
+		}
+
+		my $p = $data->{__portal}{$p_name};
+
+		# printf "    Portal (%s)\n", $p_name;
+		my($np) = grep { $_->[0] != $x || $_->[1] != $y } @$p;
+		my($np_x,$np_y,$np_i) = @$np;
+
+		if ($level == 0 && $np_i) {
+		    next; # outer level ending up at inner? = wall
+		}
+
+		$nx = $np_x;
+		$ny = $np_y;
+		$nl = $np_i ? $level-1 : $level+1;
+
+		# printf "      move to: %d,%d,%d\n", $nx, $ny, $nl;
+	    }
+	    else {
+		next;
+	    }
+	    next if $data->{__steps}[$nl]{$ny}{$nx};
+	    # printf "    Adding: %d,%d,%d\n", $nx, $ny, $nl;
+	    push @next, [ $nx, $ny, $nl ];
+	}
+    }
+    @explore = @next;
+    $steps++;
+    # sleep .1;
+}
+
+my($ex,$ey) = @{$data->{__portal}{AA}[0]};
+$solution2 = $data->{__steps}[0]{$ey}{$ex} || 'no path';
+
+printf "Solution 2: %s\n", join " ", $solution2, $max_level;
