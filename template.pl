b57d05a5e8003f97db1b8046ef2fa741dd0a0dda
Peder Stray <peder.stray@gmail.com>
2020-12-06 06:55:58 +0100

    Add a template for soultion.pl

---
+++ template.pl
@@ -0,0 +1,21 @@
+#! /usr/bin/perl
+#
+# Copyright (C) 2020 by Peder Stray <peder@ninja.no>
+#
+
+use strict;
+
+use Data::Dumper;
+use FindBin;
+eval { require "$FindBin::Bin/../../lib/common.pl"; };
+
+my $solution1 = 0;
+my $solution1 = 0;
+
+# $/ = "";
+while (<>) {
+
+}
+
+printf "Solution1: %s\n", $solution1;
+printf "Solution2: %s\n", $solution2;
